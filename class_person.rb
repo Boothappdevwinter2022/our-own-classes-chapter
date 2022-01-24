40828727f8427466f5d4363d082e984ae83e7321
thaong91 <tnguye13@chicagobooth.edu>
2022-01-24 04:51:42 +0000

    complete hw

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,19 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    require "date"
+    dob = Date.parse(self.birthdate)
+    #change the formula to diff in year because that's how the unit test was defined
+    age = Date.today.year - dob.year
+    return age
+  end
 end
